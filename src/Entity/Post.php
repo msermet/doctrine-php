eb0531e4a78fa260e1fce22196256f02f679cf68
unknown <sermetm@SIOPERGAUD.FR>
2024-09-23 14:01:27 +0200

    commit initial

---
+++ src/Entity/Post.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+#[ORM\Entity]
+#[ORM\Table(name: "posts")]
+class Post
+{
+    #[ORM\Id]   // Clé primaire dans la table posts
+    #[ORM\Column(name: 'id_post', type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int $id;
+    #[ORM\Column(name: 'titre_post',type: 'string',length: 200,nullable: false)]
+    private string $titre;
+    #[ORM\Column(name: 'contenu_post',type: 'text',nullable: false)]
+    private string $contenu;
+    #[ORM\Column(name: 'date_creation_post',type: 'datetime',nullable: false)]
+    private \DateTime $createdAt;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getTitre(): string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): void
+    {
+        $this->titre = $titre;
+    }
+
+    public function getContenu(): string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): void
+    {
+        $this->contenu = $contenu;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+
+}
